8e0f96dfbb5e87453e2bc04457b0d4bced764375
Shane Weaver <shanebweaver@gmail.com>
2025-04-03 11:03:16 -0700

    Set up connected animation

--- src/CaptureTool.UI/Xaml/Pages/DesktopCaptureOptionsPage.xaml.cs
+++ src/CaptureTool.UI/Xaml/Pages/DesktopCaptureOptionsPage.xaml.cs
@@ -1,3 +1,6 @@
+using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml.Navigation;
+
 namespace CaptureTool.UI.Xaml.Pages;
 
 public sealed partial class DesktopCaptureOptionsPage : DesktopCaptureOptionsPageBase
@@ -5,5 +8,22 @@ public sealed partial class DesktopCaptureOptionsPage : DesktopCaptureOptionsPag
     public DesktopCaptureOptionsPage()
     {
         InitializeComponent();
+    }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        ConnectedAnimation animation = ConnectedAnimationService.GetForCurrentView().GetAnimation("forwardAnimation");
+        animation?.TryStart(NewDesktopCaptureButton);
+        base.OnNavigatedTo(e);
+    }
+
+    protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+    {
+        if (e.NavigationMode == NavigationMode.Back)
+        {
+            ConnectedAnimation animation = ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("backAnimation", NewDesktopCaptureButton);
+            animation.Configuration = new DirectConnectedAnimationConfiguration();
+        }
+        base.OnNavigatingFrom(e);
     }
 }
--- src/CaptureTool.UI/Xaml/Pages/HomePage.xaml.cs
+++ src/CaptureTool.UI/Xaml/Pages/HomePage.xaml.cs
@@ -1,3 +1,6 @@
+using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml.Navigation;
+
 namespace CaptureTool.UI.Xaml.Pages;
 
 public sealed partial class HomePage : HomePageBase
@@ -5,5 +8,18 @@ public sealed partial class HomePage : HomePageBase
     public HomePage()
     {
         InitializeComponent();
+    }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        ConnectedAnimation animation = ConnectedAnimationService.GetForCurrentView().GetAnimation("backAnimation");
+        animation?.TryStart(NewDesktopCaptureButton);
+        base.OnNavigatedTo(e);
+    }
+
+    protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+    {
+        ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("forwardAnimation", NewDesktopCaptureButton);
+        base.OnNavigatingFrom(e);
     }
 }
