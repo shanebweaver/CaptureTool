a4523ec06b5be72ade9c495d8f44bee81bdd15a4
Shane Weaver <shanebweaver@gmail.com>
2025-05-15 15:02:28 -0700

    Add services test project

---
+++ src/CaptureTool.Services.Tests/NavigationTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CaptureTool.Services.Tests;
+
+[TestClass]
+public sealed class NavigationTests
+{
+    [TestMethod]
+    public void NavigateTest()
+    {
+    }
+}
