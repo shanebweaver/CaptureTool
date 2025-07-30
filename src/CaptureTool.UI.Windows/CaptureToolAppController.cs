64246c7e41654ffad92db9460dfa5b63f9e0b08b
Shane Weaver <shanebweaver@gmail.com>
2025-07-30 14:42:38 -0700

    Update version

--- src/CaptureTool.UI.Windows/CaptureToolAppController.cs
+++ src/CaptureTool.UI.Windows/CaptureToolAppController.cs
@@ -20,7 +20,6 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
