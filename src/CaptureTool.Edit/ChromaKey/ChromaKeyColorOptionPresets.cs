02d51fa6482b905b91bf82ac43aac8198a6c6896
Shane Weaver <shanebweaver@gmail.com>
2025-08-06 11:45:54 -0700

    Adjust default key colors

--- src/CaptureTool.Edit/ChromaKey/ChromaKeyColorOptionPresets.cs
+++ src/CaptureTool.Edit/ChromaKey/ChromaKeyColorOptionPresets.cs
@@ -12,15 +12,20 @@ public static partial class ChromaKeyColorOptionPresets
     public static ChromaKeyColorOption PantoneGreen { get; } = new(Color.FromArgb(10, 175, 48));
     public static ChromaKeyColorOption NorthTexasGreen { get; } = new(Color.FromArgb(0, 151, 50));
     public static ChromaKeyColorOption RoyalBlue { get; } = new(Color.RoyalBlue);
+    public static ChromaKeyColorOption ChromaGreen { get; } = new(Color.FromArgb(0, 177, 64));
+    public static ChromaKeyColorOption TVGreen { get; } = new(Color.FromArgb(0, 255, 0));
+    public static ChromaKeyColorOption DSLRGreen { get; } = new(Color.FromArgb(16, 180, 71));
+    public static ChromaKeyColorOption ChromaBlue { get; } = new(Color.FromArgb(0, 71, 187));
+    public static ChromaKeyColorOption TVBlue { get; } = new(Color.FromArgb(0, 0, 255));
+    public static ChromaKeyColorOption HollywoodBlue { get; } = new(Color.FromArgb(29, 59, 163));
 
     public static ChromaKeyColorOption[] All { get; } = [
-        SpringBud,
-        BrightGreen,
-        GreenScreenGreen,
-        NeonGreen,
-        VividMalachite,
+        ChromaGreen,
+        TVGreen,
+        DSLRGreen,
         PantoneGreen,
-        NorthTexasGreen,
-        RoyalBlue
+        ChromaBlue,
+        TVBlue,
+        HollywoodBlue
     ];
 }
