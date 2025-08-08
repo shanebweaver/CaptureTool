c7980448eee6dd70139970fac5122f113076f895
Shane Weaver <shanebweaver@gmail.com>
2025-08-08 08:09:30 -0700

    Improvements to add ons page

--- src/CaptureTool.ViewModels/AddOnsPageViewModel.cs
+++ src/CaptureTool.ViewModels/AddOnsPageViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using CaptureTool.Common.Commands;
-using CaptureTool.Core;
 using CaptureTool.Core.AppController;
 using CaptureTool.FeatureManagement;
+using CaptureTool.Services.Localization;
 using CaptureTool.Services.Navigation;
 using CaptureTool.Services.Store;
 using System.Threading;
 using System.Threading.Tasks;
+using static CaptureTool.Core.CaptureToolStoreProductIds;
 
 namespace CaptureTool.ViewModels;
 
@@ -14,6 +15,7 @@ public sealed partial class AddOnsPageViewModel : LoadableViewModelBase
     private readonly IAppController _appController;
     private readonly IStoreService _storeService;
     private readonly INavigationService _navigationService;
+    private readonly ILocalizationService _localizationService;
 
     public RelayCommand GetChromaKeyAddOnCommand => new(GetChromaKeyAddOn, () => IsChromaKeyFeatureEnabled);
     public RelayCommand GoBackCommand => new(GoBack, () => _navigationService.CanGoBack);
@@ -22,20 +24,28 @@ public sealed partial class AddOnsPageViewModel : LoadableViewModelBase
     public bool IsChromaKeyAddOnOwned
     {
         get => _isChromaKeyAddOnOwned;
-        set => Set(ref  _isChromaKeyAddOnOwned, value);
+        private set => Set(ref _isChromaKeyAddOnOwned, value);
     }
 
     private string _chromaKeyAddOnPrice = string.Empty;
     public string ChromaKeyAddOnPrice
     {
         get => _chromaKeyAddOnPrice;
-        set => Set(ref _chromaKeyAddOnPrice, value);
+        private set => Set(ref _chromaKeyAddOnPrice, value);
+    }
+
+    private bool _isChromaKeyAddOnAvailable;
+    public bool IsChromaKeyAddOnAvailable
+    {
+        get => _isChromaKeyAddOnAvailable;
+        private set => Set(ref _isChromaKeyAddOnAvailable, value);
     }
 
     public bool IsChromaKeyFeatureEnabled { get; }
 
     public AddOnsPageViewModel(
         IAppController appController,
+        ILocalizationService localizationService,
         IStoreService storeService,
         INavigationService navigationService,
         IFeatureManager featureManager)
@@ -43,24 +53,34 @@ public sealed partial class AddOnsPageViewModel : LoadableViewModelBase
         _appController = appController;
         _storeService = storeService;
         _navigationService = navigationService;
-
+        _localizationService = localizationService;
+        _chromaKeyAddOnPrice = localizationService.GetString("AddOns_ItemNotAvailable");
         IsChromaKeyFeatureEnabled = featureManager.IsEnabled(CaptureToolFeatures.Feature_ImageEdit_ChromaKey);
     }
 
     public override async Task LoadAsync(object? parameter, CancellationToken cancellationToken)
     {
-        StoreAddOn? addOn = await _storeService.GetAddonProductInfoAsync(CaptureToolStoreProductIds.AddOns.ChromaKeyBackgroundRemoval);
+        StoreAddOn? addOn = await _storeService.GetAddonProductInfoAsync(AddOns.ChromaKeyBackgroundRemoval);
         if (addOn != null)
         {
-            IsChromaKeyAddOnOwned = addOn.IsOwned;
-            ChromaKeyAddOnPrice = addOn.Price;
+            bool isOwned = addOn.IsOwned;
+            IsChromaKeyAddOnAvailable = !isOwned;
+            IsChromaKeyAddOnOwned = isOwned;
+            ChromaKeyAddOnPrice = isOwned ? _localizationService.GetString("AddOns_ItemOwned") : addOn.Price;
         }
-        
+        else
+        {
+            IsChromaKeyAddOnAvailable = false;
+            IsChromaKeyAddOnOwned = false;
+            ChromaKeyAddOnPrice = _localizationService.GetString("AddOns_ItemNotAvailable");
+        }
+
         await base.LoadAsync(parameter, cancellationToken);
     }
 
     public override void Unload()
     {
+        _isChromaKeyAddOnAvailable = false;
         _isChromaKeyAddOnOwned = false;
         _chromaKeyAddOnPrice = string.Empty;
         base.Unload();
@@ -71,8 +91,10 @@ public sealed partial class AddOnsPageViewModel : LoadableViewModelBase
         if (IsChromaKeyFeatureEnabled && !IsChromaKeyAddOnOwned)
         {
             var hwnd = _appController.GetMainWindowHandle();
-            bool success = await _storeService.PurchaseAddonAsync(CaptureToolStoreProductIds.AddOns.ChromaKeyBackgroundRemoval, hwnd);
+            bool success = await _storeService.PurchaseAddonAsync(AddOns.ChromaKeyBackgroundRemoval, hwnd);
+            IsChromaKeyAddOnAvailable = !success;
             IsChromaKeyAddOnOwned = success;
+            ChromaKeyAddOnPrice = _localizationService.GetString("AddOns_ItemOwned");
         }
     }
 
