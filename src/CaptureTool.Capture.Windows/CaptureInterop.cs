0a30ba37d0b34b4c63e72825b5bfc2a49de10722
Shane Weaver <shanebweaver@gmail.com>
2025-08-25 10:38:25 -0700

    Move CaptureInterop methods into new file

---
+++ src/CaptureTool.Capture.Windows/CaptureInterop.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CaptureTool.Capture.Windows;
+
+internal static partial class CaptureInterop
+{
+    [LibraryImport("CaptureInterop.dll", EntryPoint = "AddNumbers")]
+    public static partial int AddNumbers(int a, int b);
+}
--- src/CaptureTool.Capture.Windows/ScreenRecorder.cs
+++ src/CaptureTool.Capture.Windows/ScreenRecorder.cs
@@ -1,18 +1,10 @@
-﻿using System.Runtime.InteropServices;
-
-namespace CaptureTool.Capture.Windows;
-
-internal static partial class NativeMethods
-{
-    [LibraryImport("CaptureInterop.dll", EntryPoint = "AddNumbers")]
-    public static partial int AddNumbers(int a, int b);
-}
+﻿namespace CaptureTool.Capture.Windows;
 
 public sealed partial class ScreenRecorder
 {
     public void StartRecording()
     {
-        var result = NativeMethods.AddNumbers(3, 4);
+        var result = CaptureInterop.AddNumbers(3, 4);
         System.Diagnostics.Debug.WriteLine(result);
     }
 
