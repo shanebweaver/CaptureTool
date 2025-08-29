12a3abc5616ffc67b8c3028529b4e98e47ff8759
Shane Weaver <shanebweaver@gmail.com>
2025-08-29 08:50:22 -0700

    Continuing ScreenRecorder code

--- src/CaptureInterop/ScreenRecorder.cpp
+++ src/CaptureInterop/ScreenRecorder.cpp
@@ -7,11 +7,20 @@
 #include <windows.graphics.capture.interop.h>
 
 #include <roapi.h>
+#include <d3d11.h>
 
+using namespace ABI::Windows::Graphics::DirectX;
+using namespace ABI::Windows::Graphics::DirectX::Direct3D11;
+using namespace ABI::Windows::Graphics;
 using namespace ABI::Windows::Graphics::Capture;
 
 // Globals (later we can wrap in a struct)
 static wil::com_ptr<IGraphicsCaptureItem> g_captureItem;
+static wil::com_ptr<ID3D11Device> g_d3dDevice;
+static wil::com_ptr<ID3D11DeviceContext> g_d3dContext;
+static wil::com_ptr<IGraphicsCaptureSession> g_captureSession;
+static wil::com_ptr<IDirect3D11CaptureFramePool> g_framePool;
+
 
 // Helper: release everything cleanly
 static void CleanupInternal()
@@ -36,12 +45,13 @@ static wil::com_ptr<IGraphicsCaptureItemInterop> GetGraphicsCaptureItemInterop(H
         static_cast<UINT32>(wcslen(RuntimeClass_Windows_Graphics_Capture_GraphicsCaptureItem)),
         &classId
     );
-    if (FAILED(hr))
+    if (FAILED(hr) || classId == 0)
     {
         if (outHr) *outHr = hr;
         return nullptr;
     }
 
+    // TODO: Fix warning
     hr = RoGetActivationFactory(
         classId,
         __uuidof(IGraphicsCaptureItemInterop),
@@ -80,6 +90,48 @@ static wil::com_ptr<IGraphicsCaptureItem> GetGraphicsCaptureItemForMonitor(HMONI
     return item;
 }
 
+static HRESULT InitializeD3DDevice()
+{
+    D3D_FEATURE_LEVEL featureLevels[] = { D3D_FEATURE_LEVEL_11_0 };
+    wil::com_ptr<ID3D11Device> device;
+    wil::com_ptr<ID3D11DeviceContext> context;
+
+    HRESULT hr = D3D11CreateDevice(
+        nullptr,                    // default adapter
+        D3D_DRIVER_TYPE_HARDWARE,
+        nullptr,
+        D3D11_CREATE_DEVICE_BGRA_SUPPORT,
+        featureLevels,
+        ARRAYSIZE(featureLevels),
+        D3D11_SDK_VERSION,
+        device.put(),
+        nullptr,
+        context.put()
+    );
+    if (FAILED(hr)) return hr;
+
+    g_d3dDevice = std::move(device);
+    g_d3dContext = std::move(context);
+    return S_OK;
+}
+
+static bool TryStartCapture(HRESULT* outHr = nullptr)
+{
+    // Make sure D3D device is ready
+    HRESULT hr = InitializeD3DDevice();
+    if (FAILED(hr))
+    {
+        if (outHr) *outHr = hr;
+        return false;
+    }
+
+    // TODO: create IDirect3DDevice from g_d3dDevice
+    // TODO: create frame pool
+    // TODO: create capture session
+    // TODO: start session
+    return true;
+}
+
 // Exported API
 extern "C"
 {
@@ -87,7 +139,7 @@ extern "C"
     {
         HRESULT hr = S_OK;
 
-        wil::com_ptr<IGraphicsCaptureItemInterop> interop = GetGraphicsCaptureItemInterop();
+        wil::com_ptr<IGraphicsCaptureItemInterop> interop = GetGraphicsCaptureItemInterop(&hr);
         if (!interop)
         {
             return false;
@@ -98,6 +150,14 @@ extern "C"
         {
             return false;
         }
+
+        bool success = TryStartCapture();
+        if (!success)
+        {
+            return false;
+        }
+
+        // TODO: Do something with hr. Maybe return it?
 
         return true;
     }
