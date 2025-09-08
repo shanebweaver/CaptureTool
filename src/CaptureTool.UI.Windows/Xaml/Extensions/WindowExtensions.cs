10c0332f1931675a67a098fa5a4df91d6d5e8287
Shane Weaver <shanebweaver@gmail.com>
2025-09-08 15:33:56 -0700

    Cleanup

--- src/CaptureTool.UI.Windows/Xaml/Extensions/WindowExtensions.cs
+++ src/CaptureTool.UI.Windows/Xaml/Extensions/WindowExtensions.cs
@@ -2,6 +2,10 @@
 using Microsoft.UI.Xaml;
 using System;
 using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.Graphics.Gdi;
+using Windows.Win32.UI.WindowsAndMessaging;
 
 namespace CaptureTool.UI.Windows.Xaml.Extensions;
 
@@ -34,59 +38,57 @@ internal static partial class WindowExtensions
         CenterOnScreen(hwnd);
     }
 
-    /// <summary>
-    /// Centers the window on the current monitor
-    /// </summary>
-    /// <param name="hwnd">Window handle</param>
-    /// <param name="width">Width of the window in device independent pixels, or <c>null</c> if keeping the current size</param>
-    /// <param name="height">Height of the window in device independent pixels, or <c>null</c> if keeping the current size</param>
-    public static void CenterOnScreen(IntPtr hwnd, double? width = null, double? height = null)
+    public static void CenterOnScreen(IntPtr hwndPtr, double? width = null, double? height = null)
     {
-        var hwndDesktop = global::Windows.Win32.PInvoke.MonitorFromWindow(
-            new global::Windows.Win32.Foundation.HWND(hwnd), 
-            global::Windows.Win32.Graphics.Gdi.MONITOR_FROM_FLAGS.MONITOR_DEFAULTTONEAREST);
-        global::Windows.Win32.Graphics.Gdi.MONITORINFO info = new()
+        HWND hwnd = new(hwndPtr);
+        HMONITOR hwndDesktop = PInvoke.MonitorFromWindow(hwnd, MONITOR_FROM_FLAGS.MONITOR_DEFAULTTONEAREST);
+        MONITORINFO info = new()
         {
-            cbSize = (uint)Marshal.SizeOf<global::Windows.Win32.Graphics.Gdi.MONITORINFO>()
+            cbSize = (uint)Marshal.SizeOf<MONITORINFO>()
         };
-        global::Windows.Win32.PInvoke.GetMonitorInfo(hwndDesktop, ref info);
-        var dpi = global::Windows.Win32.PInvoke.GetDpiForWindow(new(hwnd));
-        global::Windows.Win32.PInvoke.GetWindowRect(new(hwnd), out global::Windows.Win32.Foundation.RECT windowRect);
-        var scalingFactor = dpi / 96d;
-        var w = width.HasValue ? (int)(width * scalingFactor) : windowRect.right - windowRect.left;
-        var h = height.HasValue ? (int)(height * scalingFactor) : windowRect.bottom - windowRect.top;
-        var cx = (info.rcMonitor.left + info.rcMonitor.right) / 2;
-        var cy = (info.rcMonitor.bottom + info.rcMonitor.top) / 2;
-        var left = cx - (w / 2);
-        var top = cy - (h / 2);
-        SetWindowPosOrThrow(new global::Windows.Win32.Foundation.HWND(hwnd), new global::Windows.Win32.Foundation.HWND(), left, top, w, h, 0);
+        PInvoke.GetMonitorInfo(hwndDesktop, ref info);
+
+        uint dpi = PInvoke.GetDpiForWindow(hwnd);
+        PInvoke.GetWindowRect(hwnd, out RECT windowRect);
+        
+        double scalingFactor = dpi / 96d;
+        double w = width.HasValue ? (int)(width * scalingFactor) : windowRect.right - windowRect.left;
+        double h = height.HasValue ? (int)(height * scalingFactor) : windowRect.bottom - windowRect.top;
+        double cx = (info.rcMonitor.left + info.rcMonitor.right) / 2;
+        double cy = (info.rcMonitor.bottom + info.rcMonitor.top) / 2;
+        double left = cx - (w / 2);
+        double top = cy - (h / 2);
+        SetWindowPosOrThrow(hwnd, new HWND(), (int)left, (int)top, (int)w, (int)h, 0);
     }
 
-    public static void HorizontalCenterOnScreen(IntPtr hwnd, double? width = null, double? height = null)
+    public static void HorizontalCenterOnScreen(IntPtr hwndPtr, double? width = null, double? height = null)
     {
-        var hwndDesktop = global::Windows.Win32.PInvoke.MonitorFromWindow(
-            new global::Windows.Win32.Foundation.HWND(hwnd),
-            global::Windows.Win32.Graphics.Gdi.MONITOR_FROM_FLAGS.MONITOR_DEFAULTTONEAREST);
-        global::Windows.Win32.Graphics.Gdi.MONITORINFO info = new()
+        HWND hwnd = new(hwndPtr);
+        HMONITOR hwndDesktop = PInvoke.MonitorFromWindow(hwnd, MONITOR_FROM_FLAGS.MONITOR_DEFAULTTONEAREST);
+        MONITORINFO info = new()
         {
-            cbSize = (uint)Marshal.SizeOf<global::Windows.Win32.Graphics.Gdi.MONITORINFO>()
+            cbSize = (uint)Marshal.SizeOf<MONITORINFO>()
         };
-        global::Windows.Win32.PInvoke.GetMonitorInfo(hwndDesktop, ref info);
-        var dpi = global::Windows.Win32.PInvoke.GetDpiForWindow(new(hwnd));
-        global::Windows.Win32.PInvoke.GetWindowRect(new(hwnd), out global::Windows.Win32.Foundation.RECT windowRect);
-        var scalingFactor = dpi / 96d;
-        var w = width.HasValue ? (int)(width * scalingFactor) : windowRect.right - windowRect.left;
-        var h = height.HasValue ? (int)(height * scalingFactor) : windowRect.bottom - windowRect.top;
-        var cx = (info.rcMonitor.left + info.rcMonitor.right) / 2;
-        var left = cx - (w / 2);
-        var top = windowRect.top;
-        SetWindowPosOrThrow(new global::Windows.Win32.Foundation.HWND(hwnd), new global::Windows.Win32.Foundation.HWND(), left, top, w, h, 0);
+        PInvoke.GetMonitorInfo(hwndDesktop, ref info);
+
+        uint dpi = PInvoke.GetDpiForWindow(hwnd);
+        PInvoke.GetWindowRect(hwnd, out RECT windowRect);
+
+        double scalingFactor = dpi / 96d;
+        double w = width.HasValue ? (int)(width * scalingFactor) : windowRect.right - windowRect.left;
+        double h = height.HasValue ? (int)(height * scalingFactor) : windowRect.bottom - windowRect.top;
+        double cx = (info.rcMonitor.left + info.rcMonitor.right) / 2;
+        double left = cx - (w / 2);
+        int top = windowRect.top;
+        SetWindowPosOrThrow(hwnd, new HWND(), (int)left, top, (int)w, (int)h, 0);
     }
 
-    private static void SetWindowPosOrThrow(global::Windows.Win32.Foundation.HWND hWnd, global::Windows.Win32.Foundation.HWND hWndInsertAfter, int X, int Y, int cx, int cy, global::Windows.Win32.UI.WindowsAndMessaging.SET_WINDOW_POS_FLAGS uFlags)
+    private static void SetWindowPosOrThrow(HWND hWnd, HWND hWndInsertAfter, int X, int Y, int cx, int cy, SET_WINDOW_POS_FLAGS uFlags)
     {
-        bool result = global::Windows.Win32.PInvoke.SetWindowPos(hWnd, hWndInsertAfter, X, Y, cx, cy, uFlags);
+        bool result = PInvoke.SetWindowPos(hWnd, hWndInsertAfter, X, Y, cx, cy, uFlags);
         if (!result)
+        {
             Marshal.ThrowExceptionForHR(Marshal.GetLastWin32Error());
+        }
     }
 }
