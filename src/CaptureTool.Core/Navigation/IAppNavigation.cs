4f0adab01870b16b83664b4e9569dbbe06f2f58d
Shane Weaver <shanebweaver@gmail.com>
2025-11-18 08:36:16 -0800

    tweak

--- src/CaptureTool.Core/Navigation/IAppNavigation.cs
+++ src/CaptureTool.Core/Navigation/IAppNavigation.cs
@@ -2,24 +2,23 @@
 using CaptureTool.Common.Storage;
 using System;
 
-namespace CaptureTool.Core.Navigation
-{
-    public interface IAppNavigation
-    {
-        bool CanGoBack { get; }
+namespace CaptureTool.Core.Navigation;
 
-        void GoBackOrGoHome();
-        void GoBackToMainWindow();
-        void GoHome();
-        void GoToAbout();
-        void GoToAddOns();
-        void GoToError(Exception exception);
-        void GoToImageCapture(CaptureOptions captureOptions, bool clearHistory = false);
-        void GoToImageEdit(ImageFile imageFile);
-        void GoToLoading();
-        void GoToSettings();
-        void GoToVideoCapture(NewCaptureArgs captureargs);
-        void GoToVideoEdit(VideoFile videoFile);
-        bool TryGoBack();
-    }
+public interface IAppNavigation
+{
+    bool CanGoBack { get; }
+
+    void GoBackOrGoHome();
+    void GoBackToMainWindow();
+    void GoHome();
+    void GoToAbout();
+    void GoToAddOns();
+    void GoToError(Exception exception);
+    void GoToImageCapture(CaptureOptions captureOptions, bool clearHistory = false);
+    void GoToImageEdit(ImageFile imageFile);
+    void GoToLoading();
+    void GoToSettings();
+    void GoToVideoCapture(NewCaptureArgs captureargs);
+    void GoToVideoEdit(VideoFile videoFile);
+    bool TryGoBack();
 }
