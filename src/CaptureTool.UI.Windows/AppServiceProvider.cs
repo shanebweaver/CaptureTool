3770f24e7ed7ee15e18847aab002281a09ae2a47
Shane Weaver <shanebweaver@gmail.com>
2025-12-14 09:10:32 -0800

    Move service registrations

--- src/CaptureTool.UI.Windows/AppServiceProvider.cs
+++ src/CaptureTool.UI.Windows/AppServiceProvider.cs
@@ -10,37 +10,14 @@ using CaptureTool.Domains.Edit.Implementations.Windows;
 using CaptureTool.Domains.Edit.Implementations.Windows.ChromaKey;
 using CaptureTool.Domains.Edit.Interfaces;
 using CaptureTool.Domains.Edit.Interfaces.ChromaKey;
-using CaptureTool.Services.Implementations.Cancellation;
+using CaptureTool.Services.Implementations.DependencyInjection;
 using CaptureTool.Services.Implementations.FeatureManagement;
-using CaptureTool.Services.Implementations.Globalization;
-using CaptureTool.Services.Implementations.Logging;
-using CaptureTool.Services.Implementations.Navigation;
-using CaptureTool.Services.Implementations.Settings;
-using CaptureTool.Services.Implementations.Telemetry;
-using CaptureTool.Services.Implementations.Windows.Clipboard;
-using CaptureTool.Services.Implementations.Windows.Localization;
-using CaptureTool.Services.Implementations.Windows.Share;
-using CaptureTool.Services.Implementations.Windows.Shutdown;
-using CaptureTool.Services.Implementations.Windows.Storage;
-using CaptureTool.Services.Implementations.Windows.Store;
-using CaptureTool.Services.Implementations.Windows.TaskEnvironment;
-using CaptureTool.Services.Implementations.Windows.Themes;
+using CaptureTool.Services.Implementations.Windows.DependencyInjection;
 using CaptureTool.Services.Interfaces;
 using CaptureTool.Services.Interfaces.Activation;
-using CaptureTool.Services.Interfaces.Cancellation;
-using CaptureTool.Services.Interfaces.Clipboard;
 using CaptureTool.Services.Interfaces.FeatureManagement;
-using CaptureTool.Services.Interfaces.Globalization;
 using CaptureTool.Services.Interfaces.Localization;
-using CaptureTool.Services.Interfaces.Logging;
 using CaptureTool.Services.Interfaces.Navigation;
-using CaptureTool.Services.Interfaces.Settings;
-using CaptureTool.Services.Interfaces.Share;
-using CaptureTool.Services.Interfaces.Shutdown;
-using CaptureTool.Services.Interfaces.Storage;
-using CaptureTool.Services.Interfaces.Store;
-using CaptureTool.Services.Interfaces.TaskEnvironment;
-using CaptureTool.Services.Interfaces.Telemetry;
 using CaptureTool.Services.Interfaces.Themes;
 using CaptureTool.Services.Interfaces.Windowing;
 using CaptureTool.ViewModels;
@@ -61,35 +38,17 @@ public partial class AppServiceProvider : IServiceProvider, IDisposable
         collection.AddSingleton<IFeatureManager, MicrosoftFeatureManager>();
 
         // Generic services
-        collection.AddSingleton<ICancellationService, CancellationService>();
-        collection.AddSingleton<IGlobalizationService, GlobalizationService>();
-        collection.AddSingleton<INavigationService, NavigationService>();
-        collection.AddSingleton<ISettingsService, LocalSettingsService>();
-        collection.AddSingleton<ITelemetryService, TelemetryService>();
-#if DEBUG
-        collection.AddSingleton<ILogService, DebugLogService>();
-#else
-        collection.AddSingleton<ILogService, ShortTermMemoryLogService>();
-#endif
+        collection.AddCoreServices();
 
         // Windows services
-        collection.AddSingleton<IClipboardService, WindowsClipboardService>();
-        collection.AddSingleton<IStoreService, WindowsStoreService>();
-        collection.AddSingleton<IChromaKeyService, Win2DChromaKeyService>();
-        collection.AddSingleton<IImageCanvasExporter, Win2DImageCanvasExporter>();
-        collection.AddSingleton<IImageCanvasPrinter, Win2DImageCanvasPrinter>();
-        collection.AddSingleton<IFilePickerService, WindowsFilePickerService>();
-        collection.AddSingleton<IThemeService, WindowsThemeService>();
-        collection.AddSingleton<IStorageService, WindowsStorageService>();
-        collection.AddSingleton<IJsonStorageService, WindowsJsonStorageService>();
-        collection.AddSingleton<ILocalizationService, WindowsLocalizationService>();
-        collection.AddSingleton<IShareService, WindowsShareService>();
-        collection.AddSingleton<ITaskEnvironment, WinUITaskEnvironment>(CreateWinUITaskEnvironment);
-        collection.AddSingleton<IShutdownHandler, WindowsShutdownHandler>();
+        collection.AddWindowsServices(App.Current.DispatcherQueue);
 
         // Windows domains
         collection.AddSingleton<IScreenCapture, WindowsScreenCapture>();
         collection.AddSingleton<IScreenRecorder, WindowsScreenRecorder>();
+        collection.AddSingleton<IChromaKeyService, Win2DChromaKeyService>();
+        collection.AddSingleton<IImageCanvasExporter, Win2DImageCanvasExporter>();
+        collection.AddSingleton<IImageCanvasPrinter, Win2DImageCanvasPrinter>();
 
         // Core services
         collection.AddSingleton<IActivationHandler, CaptureToolActivationHandler>();
@@ -147,10 +106,5 @@ public partial class AppServiceProvider : IServiceProvider, IDisposable
     {
         _serviceProvider.Dispose();
         GC.SuppressFinalize(this);
-    }
-
-    private static WinUITaskEnvironment CreateWinUITaskEnvironment(IServiceProvider serviceProvider)
-    {
-        return new WinUITaskEnvironment(App.Current.DispatcherQueue);
     }
 }
