0fba7149670fff340e64c8500dc627813f4206dd
Shane Weaver <shanebweaver@gmail.com>
2025-12-18 11:59:29 -0800

    Create cpp tests project

---
+++ src/CaptureInterop.Tests/CaptureInterop.Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CaptureInteropTests
+{
+	TEST_CLASS(CaptureInteropTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
