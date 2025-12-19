1a7d146c10717ee02ac118e364ed705c090b550c
Shane Weaver <shanebweaver@gmail.com>
2025-12-19 08:36:27 -0800

    Cpp tests pass

--- src/CaptureInterop.Tests/pch.h
+++ src/CaptureInterop.Tests/pch.h
@@ -8,5 +8,29 @@
 #define PCH_H
 
 // add headers that you want to pre-compile here
+#include <Windows.h>
+
+// DirectX
+#include <d3d11.h>
+#include <dxgi.h>
+
+// Media Foundation
+#include <mfapi.h>
+#include <mfobjects.h>
+#include <mfidl.h>
+#include <mfreadwrite.h>
+#include <mferror.h>
+
+// Audio
+#include <mmdeviceapi.h>
+#include <audioclient.h>
+#include <mmreg.h>
+#include <ksmedia.h>
+
+// Windows Implementation Library
+#include <wil/com.h>
+
+// Standard Library
+#include <vector>
 
 #endif //PCH_H
