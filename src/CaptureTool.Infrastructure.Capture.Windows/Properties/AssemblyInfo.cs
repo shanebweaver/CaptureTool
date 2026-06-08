5b84599704591433646d8c133db7cf0a89f65f10
Shane Weaver <shanebweaver@gmail.com>
2026-06-08 06:38:46 -0700

    Revert "Capture v2 (#297)" (#298)
    This reverts commit 55bc60d0ad83587f89170d486d674681dcaaa239.

--- src/CaptureTool.Infrastructure.Capture.Windows/Properties/AssemblyInfo.cs
+++ src/CaptureTool.Infrastructure.Capture.Windows/Properties/AssemblyInfo.cs
@@ -1,3 +1,2 @@
 using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("CaptureTool.Infrastructure.Capture.Tests.Windows")]
-[assembly: InternalsVisibleTo("CaptureTool.Infrastructure.Capture.Windows.Tests")]
