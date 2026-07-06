b741364b9e7c865f4ce6f8955d17df2d9e8f69ac
Shane Weaver <shanebweaver@gmail.com>
2026-07-05 20:54:36 -0700

    Remove metadata scanner project

--- src/CaptureTool.MetadataScanner.Windows.WinUI/Services/IWindowHandleProvider.cs
+++
@@ -1,8 +0,0 @@
-namespace CaptureTool.MetadataScanner.Windows.WinUI.Services;
-
-public interface IWindowHandleProvider
-{
-    IntPtr WindowHandle { get; }
-
-    void SetWindowHandle(IntPtr windowHandle);
-}
